bdfa75cb4c6b6dcd92847de55f5719bdeb9b9e7b
jmedina <ndgns2@gmail.com>
2023-03-09 14:34:32 -0700

    Prettier

--- index.js
+++ index.js
@@ -7,16 +7,15 @@ const mongoose = require("mongoose");
 mongoose.connect(process.env.DATABASE_URL);
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
-db.once("open", () => console.log('Connected to database'));
+db.once("open", () => console.log("Connected to database"));
 app.use(express.static("public"));
 app.use(cors());
-app.use(express.json())
-const userRouter = require('./routes/users')
+app.use(express.json());
+const userRouter = require("./routes/users");
 app.use("/users", userRouter);
 
 app.set("port", process.env.PORT || 3001);
 app.locals.title = "deadstock-api";
-
 
 app.listen(app.get("port"), () => {
   console.log(
